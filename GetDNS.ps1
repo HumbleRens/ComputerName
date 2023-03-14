2da2bf538098d402bc303c4c4354aa05a1d1820c
rensl0w <127228607+rensl0w@users.noreply.github.com>
2023-03-14 11:24:33 -0500

    Update GetDNS.ps1

--- GetDNS.ps1
+++ GetDNS.ps1
@@ -1,2 +1,29 @@
-$ipAddress = "IP here"
-Resolve-DnsName -Name $ipAddress -Type PTR -ErrorAction SilentlyContinue | Select-Object -ExpandProperty NameHost
+# Import IP addresses from text file
+$ipList = Get-Content -Path "C:\insert\location\here\ips.txt"
+
+# Create array to store results
+$results = @()
+
+# Loop through each IP address and get corresponding computer name
+foreach ($ipAddress in $ipList) {
+    
+    Write-Host "Getting computer name for IP address $ipAddress"
+    
+    $computerName = (Resolve-DnsName -Name $ipAddress -Type PTR -ErrorAction SilentlyContinue | Select-Object -ExpandProperty NameHost).TrimEnd('.')
+    
+    if ($computerName -ne $null) {
+        # Create object with IP address and computer name
+        $resultObject = New-Object -TypeName PSObject -Property @{
+            IPAddress = $ipAddress
+            ComputerName = $computerName
+        }
+
+              # Add result object to array
+        $results += $resultObject
+    } else {
+        Write-Warning "Unable to resolve computer name for IP address $ipAddress"
+    }
+}
+
+# Export results to Excel file
+$results | Export-Excel -Path "C:\insert\location\here\DNS.xlsx" -AutoSize -AutoFilter
